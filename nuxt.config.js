c5b6fae4003f06ec4a8b2c64e8a8a56368975f85
saptarshis3112 <saptarshis3112@gmail.com>
2021-06-07 10:07:42 +0530

    remove extra comments

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
 
